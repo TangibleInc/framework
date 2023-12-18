dea848f6995fca3389d969a1eb18341820590ce3
eliot-akira <me@eliotakira.com>
2023-12-18 18:23:44 +0100

    Organize individual build configs for all modules

--- preact/index.php
+++ preact/index.php
@@ -1,19 +1,16 @@
 <?php
 namespace tangible;
-use tangible\ajax;
+use tangible\preact;
 use tangible\framework;
 
-class ajax {
+class preact {
   static $state;
 }
 
-ajax::$state = (object) [
+preact::$state = (object) [
   'version' => framework::$state->version,
   'path' => __DIR__,
   'url' => untrailingslashit(plugins_url( '/', __FILE__ )),
 ];
 
 require_once __DIR__.'/enqueue.php';
-require_once __DIR__.'/nonce.php';
-require_once __DIR__.'/legacy.php';
-require_once __DIR__.'/actions.php';
