5d3d2274a2cd2768324e5608929d0c5883642954
eliot-akira <me@eliotakira.com>
2025-01-27 19:40:43 +0100

    Date module: Ensure DateCreator class is available when called before latest version loaded

--- date/index.php
+++ date/index.php
@@ -11,7 +11,13 @@ namespace tangible;
 if ( ! function_exists( 'tangible\\date' ) ) {
   function date( $arg = false ) {
     static $date;
-    if (!$date) $date = new DateCreator;
+    if (!$date) {
+      // In case this is called before latest version loaded
+      if (!class_exists('Tangible\\DateCreator')) {
+        require_once __DIR__ . '/DateCreator.php';
+      }
+      $date = new DateCreator;
+    }
     return $arg !== false
       ? call_user_func_array($date, func_get_args())
       : $date
@@ -24,15 +30,13 @@ require_once __DIR__.'/legacy.php';
 (include __DIR__ . '/module-loader.php')(new class {
 
   public $name = 'tangible_date';
-  public $version = '20250125';
+  public $version = '20250127';
 
   function load() {
-
     // Compatibility with older version of Date module
     if (!class_exists('Tangible\\DateCreator')) {
       require_once __DIR__ . '/DateCreator.php';
     }
-
     do_action($this->name . '_ready');
   }
 });
