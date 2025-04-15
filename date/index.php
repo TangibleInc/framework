6e5bd439bbd9d11f2ac60ddf1e1cd3491c3df5d0
eliot-akira <me@eliotakira.com>
2025-04-15 20:05:31 +0200

    Update version

--- date/index.php
+++ date/index.php
@@ -30,7 +30,7 @@ require_once __DIR__.'/legacy.php';
 (include __DIR__ . '/module-loader.php')(new class {
 
   public $name = 'tangible_date';
-  public $version = '20250226';
+  public $version = '20250415';
 
   function load() {
     // Compatibility with older version of Date module
