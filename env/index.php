4d52dc620d81fffb9d68dfd5f4bcb54721a95eff
eliot-akira <me@eliotakira.com>
2025-11-04 01:41:21 +0100

    Rewrite as a set of functions under namespace `tangible\env`
    - Use function and filter naming convention
    - Cache result of running all checks

---
+++ env/index.php
@@ -0,0 +1,151 @@
+<?php
+namespace tangible\env;
+
+use tangible\env;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Returns whether or not the current site is a staging site.
+ * @return boolean
+ */
+function is_staging(): bool {
+  static $is_staging;
+  return isset($is_staging) ? is_staging : (
+    $is_staging = env\run_staging_checks()
+  );
+}
+
+/**
+ * Runs all staging site checks and returns result.
+ * @return boolean
+ */
+function run_staging_checks(): bool {
+
+  static $hook = 'tangible_env_is_staging';
+  static $host;
+
+  if (!isset($host)) {
+     $host = strtolower( ( string ) wp_parse_url( home_url(), PHP_URL_HOST ) );
+  }
+
+  foreach ([
+    'wp',
+    'jetpack',
+    'txp',
+    'kinsta',
+    'rapyd',
+    'wpengine',
+    'subdomain',
+    'local',
+    'flywheel',
+  ] as $key) {
+    if ( call_user_func("tangible\\env\\is_${key}_staging", $host) ) {
+      return apply_filters( $hook, true, $key );
+    }
+  }
+
+  return apply_filters( $hook, false, 'all' );
+}
+
+function is_wp_staging() {
+  if ( ! function_exists( 'wp_get_environment_type' ) ) {
+    return false;
+  }
+  
+  $env_type = wp_get_environment_type();
+  if ( in_array( $env_type, array( 'staging', 'development', 'local' ), true ) ) { 
+    return true; 
+  } 
+
+  return false;
+}
+
+function is_jetpack_staging() {
+  if ( ! class_exists( '\Automattic\Jetpack\Status' ) ) {
+    return false;
+  }
+
+  $status = new \Automattic\Jetpack\Status();
+  if ( method_exists( $status, 'is_staging_site' ) && $status->is_staging_site() ) {
+    return true;
+  }
+
+  return false;
+}
+
+function is_txp_staging( $host ) {
+  if ( str_contains( $host, 'tangiblelaunchpad.com' ) ) {
+    return true;
+  }
+  
+  return false;
+}
+
+function is_kinsta_staging( $host ) {
+  if ( str_contains ( $host, '.kinsta.cloud' ) ) {
+    return true;
+  }
+
+  return false;
+}
+
+function is_rapyd_staging( $host ) {
+  if ( str_contains ( $host, '.rapydapps.cloud' ) ) {
+    return true;
+  }
+
+  return false;
+}
+
+
+function is_wpengine_staging( $host ) {
+  if ( str_contains ( $host, '.wpengine.com' ) ) {
+    return true;
+  }
+
+  return false;
+}
+
+function is_subdomain_staging( $host ) {
+  $staging_subdomains = [ 'staging', 'stage', 'dev', 'development', 'sandbox', 'test', 'preview' ]; 
+
+  foreach ( $staging_subdomains as $subdomain ) { 
+    $prefixed_staging_subdomain = str_contains( $host, $subdomain . '.' );
+    $nested_subdomain = str_contains( $host, '.' . $subdomain . '.' );
+    $dashed_staging_subdomain = str_contains( $host, '-' . $subdomain );
+
+    if ( $prefixed_staging_subdomain || $nested_subdomain ||  $dashed_staging_subdomain) { 
+      return true;
+    } 
+  }
+
+  return false;
+}
+
+function is_local_staging( $host ) {
+  $local_hosts = [ 'localhost', '127.0.0.1', '::1' ];
+  $local_tlds = [ '.test', '.local' ];
+
+  if ( in_array( $host, $local_hosts, true ) ) {
+    return true;
+  }
+
+  foreach ( $local_tlds as $tld ) { 
+    if ( str_ends_with( $host, $tld ) ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function is_flywheel_staging( $host ) {
+  if ( getenv( 'FLYWHEEL_CONFIG_DIR' ) ) {  
+    if ( str_contains( $host, 'preview' ) || str_contains( $host, 'flywheelsites' ) ) { 
+      return true; 
+    } 
+  }
+
+  return false;
+}
