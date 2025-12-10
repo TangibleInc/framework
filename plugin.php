8104a870f556804c728610ab4e86d15ac3f7c971
Igor Zinovyev <zinigor@gmail.com>
2025-12-10 16:30:40 +0300

    Added PSR-4 autoloader.

--- plugin.php
+++ plugin.php
@@ -9,6 +9,7 @@ use tangible\framework;
 
 define('TANGIBLE_FRAMEWORK_IS_PLUGIN', true);
 
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/index.php';
 
 add_action('plugins_loaded', function() {
